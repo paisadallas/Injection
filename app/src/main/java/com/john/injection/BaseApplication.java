439383718295acdf952626a8338e349555b30f86
paisadallas <44854381+paisadallas@users.noreply.github.com>
2022-06-27 18:49:43 -0400

    SAMT-10456 Added Dagger in grandle Created AppComponent and BaseApplication

---
+++ app/src/main/java/com/john/injection/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.john.injection;
+
+import com.john.injection.injection.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ app/src/main/java/com/john/injection/injection/AppComponent.java
@@ -0,0 +1,26 @@
+package com.john.injection.injection;
+
+import android.app.Application;
+
+import com.john.injection.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
